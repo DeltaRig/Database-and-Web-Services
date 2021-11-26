f51ebee1b81faf76f73a35f41a8b095a91cb690e
AMERICAS\Daniela_Rigoli <daniela_rigoli@dell.com>
2021-11-26 06:56:26 -0300

    initails ActionResults

--- WebApplication4/WebApplication1MVC/Controllers/HomeController.cs
+++ WebApplication4/WebApplication1MVC/Controllers/HomeController.cs
@@ -37,7 +37,30 @@ namespace WebApplication1MVC.Controllers
         public IActionResult Info(string t)
         {
             ViewData["Message"] = "You have requested info for " + t;
+            return View();
+        }
 
+        // add in the training
+        public ContentResult StudentAsString()
+        {
+            return Content("The name of the student is Walter Smith and he is in Grade 7.");
+        }
+
+        public ViewResult StudentView()
+        {
+            ViewData["Message"] = "The name of the student is <b>Walter Smith</b> and he is in Grade 7.";
+            return View();
+        }
+
+        public FileResult StudentFileResult()
+        {
+            byte[] fileBytes = System.IO.File.ReadAllBytes(@"./wwwroot/StudentData.txt");
+            string fileName = "StudentData.txt";
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+
+        public ViewResult StudentList()
+        {
             return View();
         }
     }
