f8781da1afe53617d6967c96436ccca6201de44e
AMERICAS\Daniela_Rigoli <daniela_rigoli@dell.com>
2021-12-08 09:29:11 -0300

    reestart project

---
+++ School/School/DAL/ClassDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School.DAL
+{
+    public class ClassDAL
+    {
+        private EFDemoWithMVCDBEntities db = new EFDemoWithMVCDBEntities();
+
+        public IEnumerable<Classe> GetAllClasses()
+        {
+            return DBNull.Classes;
+        }
+    }
+}
